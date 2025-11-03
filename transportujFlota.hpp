a101c7cc3c43076e2f452083ac534e5abee7bb4b
Isausage223 <kosowskipawel@yahoo.com>
2025-11-03 16:36:38 +0100

    Implement transportujFlota: loop factory create/transport/delete + count sails

--- transportujFlota.hpp
+++ transportujFlota.hpp
@@ -4,6 +4,29 @@
 
 unsigned int transportujFlota(unsigned int towar)
 {
-    // Twoja implementacja tutaj
-    return 0;
+    if (towar == 0u) {
+        return 0u;
+    }
+
+    Stocznia stocznia{};              // fabryka statków
+    unsigned int suma = 0u;           // sumarycznie przetransportowany towar
+    unsigned int liczbaZaglowcow = 0; // liczba UTWORZONYCH żaglowców
+
+    while (suma < towar) {
+        Statek* s = stocznia();       // 1) utwórz statek (Tankowiec lub Żaglowiec)
+
+        // 2) policz, czy to żaglowiec
+        if (dynamic_cast<Zaglowiec*>(s) != nullptr) {
+            ++liczbaZaglowcow;
+        }
+
+        // 3) dodaj, ile ten statek przewiózł
+        suma += s->transportuj();
+
+        // 4) zniszcz statek, aby nie było wycieku pamięci
+        delete s;
+    }
+
+    // 5) suma >= towar -> koniec, zwróć liczbę żaglowców
+    return liczbaZaglowcow;
 }
